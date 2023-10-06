ce61bf438d625d7e34d48e7fabf09f83a895869a
Rodrigo <rss.999999999@gmail.com>
2023-10-06 18:40:08 -0300

    creating methods (reverse)

--- src/Kata.java
+++ src/Kata.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class Kata {
+import java.util.Arrays;
+import static sun.security.util.ArrayUtil.reverse;
+
+public class Kata {
+    public static int[] flip(char dir, int[] arr) {
+        if (dir == 'R') {
+            Arrays.sort(arr);
+        } else if (dir == 'L') {
+            Arrays.sort(arr);
+            reverse(arr);
+        }
+        return arr;
+    }
+
+    private static void reverse(int[] arr) {
+        int temp;
+        for (int i = 0; i < arr.length / 2; i++) {
+            temp = arr[i];
+            arr[i] = arr[arr.length - (1 + i)];
+            arr[arr.length - (1 + i)] = temp;
+        }
+    }
 }
--- tests/SolutionTest.java
+++ tests/SolutionTest.java
@@ -1,11 +1,63 @@
 import org.junit.Test;
 import static org.junit.Assert.*;
 import org.junit.runners.JUnit4;
+import java.util.*;
 
 public class SolutionTest {
+    private static int[] flip(char dir, int[] arr) {
+
+        int[] rra = Arrays.copyOf(arr, arr.length);
+
+        Arrays.sort(rra);
+
+        if (dir == 'L') {
+            for (int i = 0; i < rra.length / 2; i++) {
+                int temp = rra[i];
+                rra[i] = rra[rra.length - 1 - i];
+                rra[rra.length - 1 - i] = temp;
+            }
+        }
+
+        return rra;
+    }
+
+    private static char getRandomDir()
+    {
+        Random r = new Random();
+        return r.nextInt(2) == 0 ? 'R' : 'L';
+    }
+
+    private static int[] getRandomArr()
+    {
+        Random r = new Random();
+        int size = r.nextInt(50);
+        int[] arr = new int[size];
+
+        for (int i = 0; i < size; i++)
+        {
+            arr[i] = r.nextInt(50);
+        }
+        return arr;
+    }
+
     @Test
-    public void Sample() {
-        assertArrayEquals(new int[]{ 1,2,2,3 }, Kata.flip('R', new int[]{ 3,2,1,2 }));
-        assertArrayEquals(new int[]{ 5,5,4,3,1 }, Kata.flip('L', new int[]{ 1,4,5,3,5 }));
+    public void Fixed() {
+        assertArrayEquals(new int[]{ 1,4,5,6,7 }, Kata.flip('R', new int[]{ 4,5,6,7,1 }));
+        assertArrayEquals(new int[]{ 9,8,3,2,1,0 }, Kata.flip('L', new int[]{ 3,0,9,8,1,2 }));
+        assertArrayEquals(new int[]{ 12,1,0,0,0 }, Kata.flip('L', new int[]{ 0,0,12,0,1 }));
+        assertArrayEquals(new int[]{ 2,4,7,13,93 }, Kata.flip('R', new int[]{ 13,2,4,7,93 }));
+        assertArrayEquals(new int[]{ 1,2,3,4,5 }, Kata.flip('R', new int[]{ 5,4,3,2,1 }));
+    }
+
+    @Test
+    public void Random()
+    {
+        for (int i = 0; i < 100; i++)
+        {
+            char dir = getRandomDir();
+            int[] arr = getRandomArr();
+
+            assertArrayEquals(flip(dir, arr), Kata.flip(dir, arr));
+        }
     }
 }
